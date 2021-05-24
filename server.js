38bf61492b4aa101af3545586f4bfacbdfb299ea
Sunbabaara <csunbabaara@gmail.com>
2021-05-24 16:36:27 +0000

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express= require("express");
 const dotenv =require("dotenv");
+const cors = require("cors")
 const morgan = require("morgan");
 const connectDB = require("./config/connectDB");
 const accountsRoute =require("./routes/accountsRoute");
@@ -13,6 +14,7 @@ connectDB();
 
 
 //middlewares
+app.use(cors())
 app.use(express.json());
 app.use(morgan("dev"));
 
