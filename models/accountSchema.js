daec37c9f6dac9efbeba0e0eb8a16e669c3ceb87
Sunbabaara <csunbabaara@gmail.com>
2021-05-24 12:18:01 +0000

    added new

--- models/accountSchema.js
+++ models/accountSchema.js
@@ -5,19 +5,11 @@ const accountSchema = mongoose.Schema({
         type: Number,
         required: true
     },
-    type:{
-        type: String,
-        required: true
-    },
+   
     note:{
         type: String,
         required: true
-    },
-    day:{
-        type: String,
-        required: true
     }
-    
 },{
     timestamp:true
 })
